27c795b420a3a7c18ce407790057f85e42b21cb4
daleksprinter <b1015120@fun.ac.jp>
2019-11-26 11:54:55 +0900

    initialize backend

---
+++ backend/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/gorilla/mux"
+
+	"github.com/joho/godotenv"
+)
+
+type Server struct {
+	db     *sql.DB
+	router *mux.Router
+}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+func NewDB(datasource string) (*sql.DB, error) {
+	return sql.Open("mysql", datasource)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("gorilla"))
+}
+
+func NewRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", Index)
+	return r
+}
+
+func (s *Server) Init(datasource string) {
+	db, err := NewDB(datasource)
+	if err != nil {
+		log.Fatal("failed to connect DB. %s", err)
+	}
+
+	s.db = db
+
+	s.router = NewRouter()
+}
+
+func (s *Server) Run(addr string) {
+	log.Printf("Listening on port %s", addr)
+
+	srv := &http.Server{
+		Handler: s.router,
+		Addr:    addr,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("error loading .env file. %s", err)
+	}
+	datasource := os.Getenv("DATABASE_DATASOURCE")
+	if datasource == "" {
+		log.Fatal("Cannot get datasource for database.")
+	}
+
+	s := NewServer()
+	s.Init("root:password@tcp(0.0.0.0:3306)/share_pos")
+	s.Run("127.0.0.1:8080")
+}
