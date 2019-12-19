d67a5bf667f2f7501116b1ffbcfcc18444de2abe
daleksprinter <b1015120@fun.ac.jp>
2019-12-19 22:17:35 +0900

    [backend]add get_card_by_room_id_and_category handler

--- backend/controller/card.go
+++ backend/controller/card.go
@@ -35,3 +35,19 @@ func (c *Card) GetCardByRoomIDHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(res)
 
 }
+
+func(c *Card) GetCardByRoomIDAndCategoryHandler(w http.ResponseWriter, r*http.Request){
+	vars := mux.Vars(r)
+	room := vars["room_id"]
+	category := vars["category_id"]
+
+	roomID, _ := strconv.Atoi(room)
+	categoryID, _ := strconv.Atoi(category)
+
+	cards, _ := repository.GetCardByRoomIDAndCategory(c.db, roomID, categoryID)
+
+	res, _ := json.Marshal(cards)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
+}
--- backend/repository/card.go
+++ backend/repository/card.go
@@ -13,3 +13,11 @@ func GetCardByRoomID(db *sqlx.DB, RoomID int) ([]model.Card, error) {
 	}
 	return cards, nil
 }
+
+func GetCardByRoomIDAndCategory(db *sqlx.DB, RoomID int, CategoryID int) ([]model.Card, error) {
+	cards := make([]model.Card, 0)
+	if err := db.Select(&cards, `select * from post where room_id = ? and category_id = ?`, RoomID, CategoryID); err != nil {
+		return nil, err
+	}
+	return cards, nil
+}
