ed93ea673b03342dfadf91dbd4eb1f0301465818
daleksprinter <g2119022@fun.ac.jp>
2020-02-22 17:16:50 +0900

    cut out new room component

--- frontend/app/src/components/Join.js
+++ frontend/app/src/components/Join.js
@@ -1,80 +1,14 @@
 import React, {Component} from 'react';
 import { withRouter } from 'react-router';
 import JoinRoom from './JoinRoom'
+import NewRoom from './NewRoom'
 class Join extends Component{
-    constructor(){
-        super();
-        this.state = {
-            room_id_new: "",
-            password_new: "",
-            isprivate: true,
-
-        }
-    }
-
-    handlechangenew = (e) => {
-        switch(e.target.id){
-            case 'roomname_new':
-                this.setState({
-                    room_id_new: e.target.value,
-                })
-                break
-            case 'password_new':
-                this.setState({
-                    password_new: e.target.value,
-                })
-                break
-	    default :
-		break
-        }
-    }
-
-    handleclicknew = (e) => {
-        const url = `/rooms`
-        const data = {
-            room_name: this.state.room_id_new,
-            is_private: this.state.isprivate,
-            hashed_password: (this.state.isprivate ? this.state.password_new : "")
-        }
-        fetch(url, {
-            method: "POST",
-            headers: {
-                'Content-type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        }).then(res => {
-		if (!res.ok){
-			 throw Error(res.statusText);
-		}
-		return res.text()
-        }).then(txt => {
-            this.props.history.push(`/rooms/${this.state.room_id_new}`)
-	}).catch(err => {
-		console.log(err)
-	})
-    }
-
-
-    handlecheck = (e) => {
-        this.setState({
-            isprivate: !this.state.isprivate
-        })
-    }
-
-    
 
     render(){
-	console.log(this.props)
         return(
             <div>
                 <div>this is home page</div>
-
-                <div>new room</div>
-                <input type = "text" onChange = {this.handlechangenew} placeholder = 'room name' id = "roomname_new"></input>
-                <input type = "checkbox" onChange = {this.handlecheck} checked = {this.state.isprivate}></input>
-                {this.state.isprivate ? <input type = 'password' onChange = {this.handlechangenew} placeholder = 'password' id = "password_new"></input> : <span />}
-                <button type = "button" onClick = {this.handleclicknew}>create</button>
-
+		<NewRoom history={this.props.history} />
 		<JoinRoom history={this.props.history}/>
 	    </div>
        )
---
+++ frontend/app/src/components/NewRoom.js
@@ -0,0 +1,62 @@
+import React, {useState} from 'react';
+import { withRouter } from 'react-router';
+
+function NewRoom(props){
+	const [room_name_new, set_room_name_new] = useState("")
+	const [room_password_new, set_room_password_new] = useState("")
+	const [is_private, set_is_private] = useState(false)
+
+	const    handlechangenew = (e) => {
+		switch(e.target.id){
+			case 'roomname_new':
+				set_room_name_new(e.target.value)
+				break
+			case 'password_new':
+				set_room_password_new(e.target.value)
+				break
+			default :
+				break
+		}
+	}
+
+	const handleclicknew = (e) => {
+		const url = `/rooms`
+		const data = {
+			room_name: room_name_new,
+			is_private: is_private,
+			hashed_password: (is_private ? room_password_new : "")
+		}
+		fetch(url, {
+			method: "POST",
+			headers: {
+				'Content-type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		}).then(res => {
+			if (!res.ok){
+				throw Error(res.statusText);
+			}
+			return res.text()
+		}).then(txt => {
+			props.history.push(`/rooms/${room_name_new}`)
+		}).catch(err => {
+			console.log(err)
+		})
+	}
+
+
+	const handlecheck = (e) => set_is_private(!is_private)
+
+	return(
+		<div>
+			<div>new room</div>
+			<input type = "text" onChange = {handlechangenew} placeholder = 'room name' id = "roomname_new"></input>
+			<input type = "checkbox" onChange = {handlecheck} checked = {is_private}></input>
+			{is_private ? <input type = 'password' onChange = {handlechangenew} placeholder = 'password' id = "password_new"></input> : <span />}
+			<button type = "button" onClick = {handleclicknew}>create</button>
+		</div>
+
+	)
+}
+
+export default NewRoom
