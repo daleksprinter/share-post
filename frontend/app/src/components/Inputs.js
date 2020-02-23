6aec41a87d8c96f47484957d49a8b4926bb8840b
daleksprinter <g2119022@fun.ac.jp>
2020-02-23 14:54:29 +0900

    make input area position fixed

--- frontend/app/src/components/Inputs.js
+++ frontend/app/src/components/Inputs.js
@@ -2,17 +2,29 @@ import React, {useState} from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { addArea } from '../actions/Input'
 import InputTxt from './InputTxt'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+	root: {
+		backgroundColor: 'black',
+		position: 'fixed',
+		left: '50%',
+		top: '80%',
+	},
+})
+
 const inputselector = (state) => state.inputs
 function Inputs(props) {
 	const dispatch = useDispatch();
 	const inputs = useSelector(inputselector)
-	console.log(inputs)
+
 	const handleclick = () => {
 		dispatch(addArea())
 	}
+	
+	const classes = useStyles()
 	return(
-
-		<div>
+		<div className = {classes.root}>
 			{inputs.inputs.map((e, i) => {
 				return(
 					<InputTxt id = {i} txt = {e} />
