4c83de4fd77b8c4f3d21099f6a31a89bd9c3471a
daleksprinter <g2119022@fun.ac.jp>
2020-02-25 13:55:48 +0900

    add post category handler

--- backend/src/controller/category.go
+++ backend/src/controller/category.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 
 	"fmt"
+	"github.com/daleksprinter/share-post/auth"
+	"github.com/daleksprinter/share-post/model"
 	"github.com/daleksprinter/share-post/repository"
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
@@ -41,5 +43,38 @@ func (c *Category) GetCategoriesByRoomNameHandler(w http.ResponseWriter, r *http
 	res, _ := json.Marshal(cards)
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(res)
+
+}
+
+func (c *Category) PostCategoryHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	roomName := vars["roomname"]
+
+	p := struct {
+		categoryName string `json:"category_name"`
+	}{}
+	json.NewDecoder(r.Body).Decode(&p)
+
+	usr, err := auth.GetRequestedUser(c.db, r)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	room, err := repository.GetRoomByName(c.db, roomName)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	cat := model.Category{
+		Title:       p.categoryName,
+		CreatedUser: usr.ID,
+		RoomID:      room.ID,
+	}
+
+	fmt.Printf("%+v/n", cat)
 
 }
