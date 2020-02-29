ae9d274b2584d245ddde2451558fd265b492aa0b
daleksprinter <g2119022@fun.ac.jp>
2020-02-29 15:47:54 +0900

    add DOMs in profile component

--- frontend/app/src/components/Profile.js
+++ frontend/app/src/components/Profile.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
-
+import React, { useEffect, useState } from 'react';
+import none from '../assets/img/google.png'
 
 function Profile(props){
+	const [icon_url, set_icon_url] = useState("")
+	const [username, set_username] = useState("")
 
 	useEffect(() => {
 		const url = "/usr"
@@ -12,6 +14,8 @@ function Profile(props){
 			return res.json()
 		}).then(json => {
 			console.log(json)
+			set_icon_url(json.Icon.String)
+			set_username(json.nickname)
 		}).catch(err => {
 			console.log(err)
 		})
@@ -19,8 +23,14 @@ function Profile(props){
 	}, [])
 
 	return(
-		<div>profile</div>
+		<div>
+			{icon_url === "" ? <img src = {none} /> : <img src = {`https://share-pos.s3-ap-northeast-1.amazonaws.com/profile/${icon_url}`} />}
+			<div>
+				{username}
+			</div>
+		</div>
 	)
+
 }
 
 export default Profile;
